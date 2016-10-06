161b7fa3284e78e397fad38c5c0aacd1461ac894
Bonnie Brewer <bj.brewer@att.net>
2016-10-06 23:07:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function iterativeLog(array) {
+  //array.forEach(console.log(`${index}: ${element}`))
+}
+
+funtion iterate(callback) {
+  var array=[123,456,789]
+  array.forEach(interativeLog);
+  return array;
+}
+
+function doToArray(array,callback) {
+  array.forEach(callback)
+}
